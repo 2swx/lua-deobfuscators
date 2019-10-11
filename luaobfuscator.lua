84822d87c1e9cb4dff7cf5a4e045c053bb92ae16
Harry Kerr <33969918+NoSharp@users.noreply.github.com>
2019-10-11 16:29:27 +0100

    Update luaobfuscator.lua

--- luaobfuscator.lua
+++ luaobfuscator.lua
@@ -1,9 +1,27 @@
---DECRYPT_3 
+--require("bit")
+bit = {}
+function bit:bxor(a,b)--Bitwise xor
+    local p,c=1,0
+    while a>0 and b>0 do
+        local ra,rb=a%2,b%2
+        if ra~=rb then c=c+p end
+        a,b,p=(a-ra)/2,(b-rb)/2,p*2
+    end
+    if a<b then a=b end
+    while a>0 do
+        local ra=a%2
+        if ra>0 then c=c+p end
+        a,p=(a-ra)/2,p*2
+    end
+    return c
+end
+
+
 local a = _G 
 local str = _G['string']
-local xor = _G['bit']['bxor']
+local aaa = _G['bit']['bxor']
 local function obf(arg1) 
-  print(arg1)
+  
   if str['len'](arg1) == 0 then
     return arg1
   end 
@@ -12,12 +30,20 @@ local function obf(arg1)
   local zero = 0
   --zero
   for v in str['gmatch'](arg1,'&') do 
-    if v == 'T' then
+    print(v)
+    if v == '|' then
+     
       start = start..str['char'](_G['bit']['bxor'](zero,108))
       zero = 0 
     else 
+
     zero = zero +1
     end
   end
   return start
 end 
+
+local res = obf("a6|aaa|||a|aaaaaaaaaaaaaaaaaaaaaaaaaT|aaaaaaaaa|a|aaaaaaaaaa||aa8u|aaaaaaaaaaaaaaaaaaaaaaaaa22|")
+print(res)
+--local resOfAA = aaaaaaa("a6baaabbbabaaaaaaaaaaaaaaaaaaaaaaaaaTbaaaaaaaaababaaaaaaaaaabbaa8ubaaaaaaaaaaaaaaaaaaaaaaaaa22b")
+--res(resOfAA)
