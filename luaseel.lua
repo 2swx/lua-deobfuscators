0203a6ee63dee5ac6022f4ae3936c0f4595a7efd
Harry Kerr <33969918+NoSharp@users.noreply.github.com>
2019-10-11 14:42:20 +0100

    Help you with being a skiddy.

---
+++ luaseel.lua
@@ -0,0 +1,26 @@
+local text = [==[
+  function IllIlllIllIlllIlllIlllIll(IllIlllIllIllIll) if (IllIlllIllIllIll==(((((919 + 636)-636)*3147)/3147)+919)) then return not true end if (IllIlllIllIllIll==(((((968 + 670)-670)*3315)/3315)+968)) then return not false end end; local IIllllIIllll = (7*3-9/9+3*2/0+3*3);local IIlllIIlllIIlllIIlllII = (3*4-7/7+6*4/3+9*9);local IllIIIllIIIIllI = table.concat;function IllIIIIllIIIIIl(IIllllIIllll) function IIllllIIllll(IIllllIIllll) function IIllllIIllll(IllIllIllIllI) end end end;IllIIIIllIIIIIl(900283);function IllIlllIllIlllIlllIlllIllIlllIIIlll(IIlllIIlllIIlllIIlllII) function IIllllIIllll(IllIllIllIllI) local IIlllIIlllIIlllIIlllII = (9*0-7/5+3*1/3+8*2) end end;IllIlllIllIlllIlllIlllIllIlllIIIlll(9083);local IllIIllIIllIII = loadstring;local IlIlIlIlIlIlIlIlII = {'\45','\45','\47','\47','\32','\68','\101','\99','\111','\109','\112','\105','\108','\101','\100','\32','\67','\111','\100','\101','\46','\32','\10','\32','\32','\112','\114','\105','\110','\116','\40','\34','\72','\101','\108','\108','\111','\32','\119','\111','\114','\108','\100','\34','\41','\10',}IllIIllIIllIII(IllIIIllIIIIllI(IlIlIlIlIlIlIlIlII,IIIIIIIIllllllllIIIIIIII))()
+]==]
+--[[
+From lua wiki
+]]--
+function split (inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t={}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+--[[
+  This will actually deobfuscate the text.
+]]--
+function deobf(text)
+  for w in text:gmatch("(IlIlIlIlIlIlIlIlII = {.*})") do
+    loadstring(w)()
+    print(table.concat(IlIlIlIlIlIlIlIlII, nil))
+  end
+end
+deobf(text)
